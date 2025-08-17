b9cf534d1c302e9b697cbabd05722e7b1f6ac41c
poprock49 <44791147+poprock49@users.noreply.github.com>
2025-08-17 10:59:07 +0900

    Add files via upload

---
+++ okipy02.py
@@ -0,0 +1,6 @@
+sale = int(input("number,please"))
+
+if sale >= 100 :
+    print("goo!")
+    
+print("end")
---
+++ okipy03.py
@@ -0,0 +1,10 @@
+sale = int(input("number, please"))
+
+if sale >= 100 :
+    print("good")
+elif sale >= 50 :
+    print("better")
+else:
+    print("low")
+    
+print("end")
